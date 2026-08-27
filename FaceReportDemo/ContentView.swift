20c6615e34f12b0e8612ab8378c72200b35e213f
Sayyed Shah <sayyed.ahmed.shah@googlemail.com>
2026-08-27 14:53:43 +0100

    Initial Commit

---
+++ FaceReportDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceReportDemo
+//
+//  Created by Sayyed on 27/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceReportDemo/FaceReportDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceReportDemoApp.swift
+//  FaceReportDemo
+//
+//  Created by Sayyed on 27/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct FaceReportDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
